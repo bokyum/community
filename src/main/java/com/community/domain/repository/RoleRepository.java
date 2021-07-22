0a447d508baab70cfa1a338b0a47f65222d5a72b
bokyum <95bogyeom@gmail.com>
2021-07-23 01:57:06 +0900

    token base login

---
+++ src/main/java/com/community/domain/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.community.domain.repository;
+
+import com.community.domain.ERole;
+import com.community.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
